a97b6fee30e06aa1c630a4f79ff1b9c2c8e187b6
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-04-12 07:49:54 -0700

    1. Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Fabric script that generates .tgz archive"""
+
+from fabric.api import local
+from datetime import datetime
+
+from fabric.decorators import runs_once
+
+
+@runs_once
+def do_pack():
+    """generates the .tgz archive from the contents of the web_static
+    folder"""
+    local("mkdir -p versions")
+    path = ("versions/web_static_{}.tgz"
+            .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
+    result = local("tart -cvzf {} web_static"
+                    .format(path))
+
+    if result.failed:
+        return None
+    return path

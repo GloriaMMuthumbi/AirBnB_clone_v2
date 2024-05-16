82668e76d984e4f0d1b2373585748ebf644f7fa7
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-05-16 01:14:53 -0700

    7. Improve engines

--- models/state.py
+++ models/state.py
@@ -10,23 +10,23 @@ from sqlalchemy.orm import relationship
 class State(BaseModel, Base):
     """ State class / table model"""
     __tablename__ = 'states'
-    if storage_t == 'db':
+    if storage_t == "db":
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
     else:
         name = ''
 
+
         @property
         def cities(self):
-            '''returns the list of City instances with state_id
-                equals the current State.id
-                FileStorage relationship between State and City
-            '''
-            from models import storage
-            related_cities = []
-            cities = storage.all(City)
-            for city in cities.values():
+            """
+            Getter method to return the list of City
+            objects linked to the current State.
+            """
+            from models import City
+            city_list = []
+            for city in storage.all(City).values():
                 if city.state_id == self.id:
-                    related_cities.append(city)
-            return related_cities
+                    city_list.append(city)
+            return city_list
